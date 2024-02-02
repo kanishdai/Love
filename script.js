15fbff513eb1af60ce1a75ad09f32a83c3d5c9bf
kanishdai <110452799+kanishdai@users.noreply.github.com>
2024-02-02 11:01:56 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,44 @@
-const noButton = document.getElementById("no-btn");
-const yesButton = document.getElementById("yes-btn");
-const popup = document.getElementById("popup");
+function startQuestions() {
+    document.getElementById("questions").style.display = "block";
+  }
+  const noButton = document.getElementById("no-btn");
+  
+  function answerYes() {
+    document.getElementById("questions").style.display = "none";
+      document.getElementById("que").style.display = "none";
+      document.getElementById("init").innerText = "YEY YEY 🎉🎉🎉";
+    document.getElementById("message").style.display = "block";
+    document.getElementById("messageText").innerText = "Awwwwwww!!!! I love you more :D <3";
+    showConfetti()
+}
+  
+  function answerNo() {
+    document.getElementById("questions").style.display = "none";
+    document.getElementById("que").style.display = "none";
+    document.getElementById("init").innerText = "YEY YEY 🎉🎉🎉";
+    document.getElementById("message").style.display = "block";
+    document.getElementById("messageText").innerText = "No worries, love takes time. But can we spend some time together and see if something beautiful blossoms?";
+    showConfetti()
 
+}
+  
 noButton.addEventListener("mouseover", () => {
-    noButton.style.position = "absolute";
-    noButton.style.left = Math.random() * 80 + "vw";
-    noButton.style.top = Math.random() * 80 + "vh";
+  noButton.style.position = "absolute";
+  noButton.style.left = Math.random() * 80 + "vw";
+  noButton.style.top = Math.random() * 80 + "vh";
 });
-
-yesButton.addEventListener("click", () => {
-    popup.style.display = "block";
-});
-
-popup.addEventListener("click", () => {
-    popup.style.display = "none";
-});
-
-// Hide the popup initially
-popup.style.display = "none";
+// Function to show confetti
+function showConfetti() {
+    const confettiSettings = {
+      target: 'confetti-canvas',
+      respawn: true,
+      colors: ['#ff0000', '#00ff00', '#0000ff'], 
+    };
+  
+    const confetti = new ConfettiGenerator(confettiSettings);
+    confetti.render();
+    setTimeout(() => {
+      confetti.clear();
+    }, 5000);
+  }
+  
